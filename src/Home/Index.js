bf2ddcfc31d034a32f2aa29551942562bd06a2b5
Mayra Sanchez <mayra.alejandra.sanchez@correounivalle.edu.co>
2024-04-14 18:55:23 -0500

    First commit

---
+++ src/Home/Index.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from "react";
+import "../Home/Home.css";
+
+//Funcion principal
+function Home() {
+
+  return (
+
+    <h1>hola</h1>
+  );
+}
+
+export default Home;
