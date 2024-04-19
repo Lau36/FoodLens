43162392eb8ab87d8d8bece709888e49595b6f25
Lau36 <laura.jaimes@correounivalle.edu.co>
2024-04-18 19:25:45 -0500

    styles

--- src/Picture/Index.js
+++ src/Picture/Index.js
@@ -1,9 +1,30 @@
-import "../Picture/Picture.css";
 import Navbar from "../Components/Navbar";
+import defaultImage from "../Resources/dafaultImage.png";
+import recipeImage from "../Resources/recipeImage.png";
+import "../Picture/Picture.css";
 
 function Picture() {
   return (
-    <Navbar  />
+    <>
+      <Navbar />
+      <div className="containerPageImages">
+        <div className="container1">
+          <div className="image">
+            <img src={defaultImage} alt="Logo" className="defaultImage" />
+          </div>
+          <div className="buttons">
+            <button className="buttonTakePicture">Tomar foto</button>
+            <button className="buttonUpload">Subir foto</button>
+          </div>
+        </div>
+        <div className="container2">
+          <button className="createRecipe">
+            <img src={recipeImage} alt="Logo" className="recipeImage" />
+            Generar reseta
+          </button>
+        </div>
+      </div>
+    </>
   );
 }
 
